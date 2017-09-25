fdd897804820c2190177583fc88723be8c06b084
Li <WLi@aeroflex.corp>
2017-09-25 11:06:43 +0100

    add second solution on TwoSum

--- code/1_two_sum.cpp
+++ code/1_two_sum.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> um;
+//        Solution 1 //////////////////////////////////////	  
+//        unordered_map<int,int> um;
+//        vector<int> res;
+//        for(int i=0;i<nums.size();++i){
+//            um[nums[i]]=i;
+//        }
+//        unordered_map<int, int>::iterator it;
+//        for(int i=0;i<nums.size();++i){
+//            it=um.find(target-nums[i]);
+//            if(it!=um.end()&&(it->second!=i)){
+//                res.push_back(i);
+//                res.push_back(it->second);
+//                return res;
+//            }
+//                
+//        }
+//
+//       Solution 2 //////////////////////////////////////
+        vector<pair<int, int>> vp;
         vector<int> res;
         for(int i=0;i<nums.size();++i){
-            um[nums[i]]=i;
+            vp.push_back(make_pair(nums[i],i));
         }
-        unordered_map<int, int>::iterator it;
-        for(int i=0;i<nums.size();++i){
-            it=um.find(target-nums[i]);
-            if(it!=um.end()&&(it->second!=i)){
-                res.push_back(i);
-                res.push_back(it->second);
-                return res;
+        sort(vp.begin(),vp.end());
+        sort(nums.begin(), nums.end());
+        int l=0, r=nums.size()-1;
+        for(l=0; l<nums.size(); ++l){
+            if(l==0||(l>0&&nums[l]!=nums[l-1])){
+                while((l<r)&&(nums[l]+nums[r]>target)) r--;
+                if(nums[l]+nums[r]==target){
+                    res.push_back(vp[l].second);
+                    res.push_back(vp[r].second);
+                    return res;
+                }
             }
-                
-        }
+        }       
+        return res;
+
     }
 };
